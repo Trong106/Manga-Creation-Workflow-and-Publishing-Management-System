5c7bbca6997f785348ef2cbb7b22b099f296932a
SWP391_FE <phanminhtuan12a7.c3hn@gmail.com>
2026-05-29 11:45:45 +0700

    update auth getseries uploadpage getdashboardstats

--- swp-be-phan-minh-tuan/MangaStudio.Backend/MangaStudio.Backend/Data/AppDbContext.cs
+++ swp-be-phan-minh-tuan/MangaStudio.Backend/MangaStudio.Backend/Data/AppDbContext.cs
@@ -1,18 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MangaStudio.Backend.Models.Entities;
 
 namespace MangaStudio.Backend.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : MangaStudioContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {
     }
-
-    public DbSet<User> Users { get; set; }
-
-    public DbSet<MangaSeries> MangaSeries { get; set; }
-
-    public DbSet<MangaPage> MangaPages { get; set; }
 }
