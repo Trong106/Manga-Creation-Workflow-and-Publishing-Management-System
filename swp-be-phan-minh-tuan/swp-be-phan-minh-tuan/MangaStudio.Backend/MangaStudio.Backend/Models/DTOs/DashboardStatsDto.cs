a9c1022be57cc2d542d33efb5dfce786f059aa44
SWP391_FE <phanminhtuan12a7.c3hn@gmail.com>
2026-05-31 10:10:26 +0700

    Update backend folder structure and add gitignore for backend

--- swp-be-phan-minh-tuan/swp-be-phan-minh-tuan/MangaStudio.Backend/MangaStudio.Backend/Models/DTOs/DashboardStatsDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MangaStudio.Backend.Models.DTOs;
-public class DashboardStatsDto
-{ 
-    public int TotalSeries { get; set; }
-    public int TotalAssistants { get; set; }
-}
--- swp-be-phan-minh-tuan/swp-be-phan-minh-tuan/MangaStudio.Backend/MangaStudio.Backend/Models/DTOs/MangaSeriesDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MangaStudio.Backend.Models.DTOs;
-public class MangaSeriesDto 
-{
-    public Guid Id { get; set; } 
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string CoverImageUrl { get; set; }
-}
